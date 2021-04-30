95005911e6d46e7d157f40ea77a17c2c28703425
Azizul Bappy <azizulbappy6@gmail.com>
2021-04-30 23:04:40 +0600

    grunt-2 test

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,12 @@
 
 module.exports = (grunt) => {
   require("time-grunt")(grunt);
-
-  require("jit-grunt")(grunt);
+  require("jit-grunt")(grunt, {
+    useminPrepare: "grunt-usemin"
+  });
 
   grunt.initConfig({
+    // dev purpose
     sass: {
       options: {
         implementation: require("node-sass"),
@@ -33,6 +35,7 @@ module.exports = (grunt) => {
         },
       },
     },
+    // build purpose
     clean: {
       build: {
         src: ["build/"],
@@ -59,10 +62,70 @@ module.exports = (grunt) => {
         ],
       },
     },
+    useminPrepare: {
+      foo: {
+        dest: "build",
+        src: ["index.html", "contactus.html", "aboutus.html"]
+      },
+      options: {
+        flow: {
+          steps: {
+            css: ["cssmin"],
+            js: ["uglify"]
+          }
+        }
+      }
+    },
+    usemin: {
+      html: ['build/index.html', 'build/contactus.html', 'build/aboutus.html'],
+      options: {
+        assetsDirs: ['build', 'build/css', 'build/js']
+      }
+    },
+    htmlmin: {
+      build: {
+        options: {
+          collapseWhitespace: true
+        },
+         files: {
+           "build/index.html": "index.html",
+           "build/contactus.html": "contactus.html",
+           "build/aboutus.html": "aboutus.html",
+         }
+      }
+    },
+    concat: {
+      options: {
+        separator: ";"
+      },
+      dist: {}
+    },
+    uglify: {
+      dist: {}
+    },
+    cssmin: {
+      dist: {}
+    }
+    
+
+
   });
 
+  // development
   grunt.registerTask("default", ["browserSync", "watch"]);
-  grunt.registerTask("css", ["sass"]);
+
+  // build --- production
   grunt.registerTask("del", ["clean"]);
   grunt.registerTask("image", ["imagemin"]);
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "imagemin",
+    'useminPrepare',
+    'concat',
+    'cssmin',
+    'uglify',
+    'usemin',
+    'htmlmin'
+  ]);
 };
