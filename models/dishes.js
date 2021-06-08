9ad8e86295df721ec9506ff71dacafee7bbbd38d
Azizul Bappy <azizulbappy6@gmail.com>
2021-06-08 23:13:07 +0600

    restful api

---
+++ models/dishes.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose")
+require("mongoose-currency").loadType(mongoose)
+
+var Currency = mongoose.Types.Currency
+
+const CommentSchema = new mongoose.Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+})
+
+const DishSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ""
+    },
+    price: {
+        type: Currency,
+        min: 0,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    comments: [CommentSchema]
+},{
+    timestamps: true
+});
+
+module.exports = mongoose.model("Dish", DishSchema)
